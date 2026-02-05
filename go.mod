b84198f9780299bfdf8b632298a50b064a63516a
Gizzmonauta <gizzmonauta@hotmail.com>
2026-02-04 22:01:13 -0500

    INITIAL: Initial commit to start the main branch

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Gizzmonauta/pokedex_go
+
+go 1.25.7
---
+++ repl_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input: "Squirtle Wartortle Blastoise",
+			expected: []string{"squirtle", "wartortle", "blastoise"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) returned %d words, expected %d", c.input, len(actual), len(c.expected))
+		}
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%q)[%d] = %q, expected %q", c.input, i, word, expectedWord)
+			}
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+		}
+	}
+}
